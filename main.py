f5ec2d8c9602030a3226ac41ebfc8c5c9b2fd016
Misha Ship <monteship@gmail.com>
2023-02-23 02:48:26 +0200

    timezone used

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import datetime
 from threading import Thread
 import time
 import schedule
-import timeit
+from pytz import timezone
 
 from config import WEBHOOK_PLAYERS, PLAYERS_EMBED, DAY_START_EMBEDS, WEBHOOK_DAY, \
     DAY_START_EMBEDS, DAY_END_EMBEDS, WEBHOOK_SQUADRONS, SQUADRONS_PARSING_TIME, SQUAD_EMBEDS
@@ -37,15 +38,16 @@ def parsing_squadrons_thread():
 
 
 def time_checker():
-    hours = time.gmtime().tm_hour + 2
-    minutes = time.gmtime().tm_min
-    curr_time = str(hours * 60 + minutes)
-    print(curr_time)
-    if curr_time in SQUADRONS_PARSING_TIME:
+    kiev_time = datetime.datetime.now(timezone('Europe/Kiev'))
+    hours = kiev_time.hour
+    minutes = kiev_time.minute
+    current_time = str(hours * 60 + minutes)
+    print(current_time)
+    if current_time in SQUADRONS_PARSING_TIME:
         parsing_squadrons_thread()
-    elif curr_time == '960':  # Run at 16:00 GMT+2
+    elif current_time == '960':  # Run at 16:00 GMT+2
         parsing_squadrons_day_start_thread()
-    elif curr_time in ['1443']:  # Run at 00:05 GMT+2 next day
+    elif current_time in ['1443']:  # Run at 00:05 GMT+2 next day
         parsing_squadrons_day_end_thread()
     parsing_players_thread()  # Run every 1 minute
 
