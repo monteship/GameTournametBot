3eec3d0f00d6895873e9b4f560cf946244df7b7e
monteship <monteship@gmail.com>
2024-10-31 01:08:32 +0200

    fix: specify .env path

--- wt_stats_scraper/settings.py
+++ wt_stats_scraper/settings.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 WEBHOOK_PLAYERS = os.environ.get("WEBHOOK_PLAYERS")
 WEBHOOK_SQUADRONS = os.environ.get("WEBHOOK_SQUADRONS")
